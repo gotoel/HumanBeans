62d3b95496311395ab0487c2b1016c6eacda8e37
gotoel <ghostaholic_1@hotmail.com>
2015-01-29 19:25:57 -0500

    BeanLife.cs - Class commented.
    Fully commented out the BeanLife class for future reference.

--- Assets/BeanLife.cs
+++ Assets/BeanLife.cs
@@ -40,14 +40,11 @@ public class BeanLife : MonoBehaviour {
 	public House house;
 	public bool hasHouse;
 
-    public AudioClip[] audioClip;
-
-
-	//GUIa
-	//Vector2 offset =  Vector2(0, 1.5);
-
+	// On creation of bean object
 	void Start () {
 
+		// bean spawns homeless, will add setting of a home so that
+		// a baby bean's parents home gets assigned to it.
 		house = null;
 		hasHouse = false;
 
@@ -82,15 +79,20 @@ public class BeanLife : MonoBehaviour {
 
 		// resize the 2d collidor to fit the baby sprite
 		resizeCollider ();
-
+		
 		//blockMaterial = 13;
 	
 
 	}
-	
+
+
+	// called every phyisics engine update
 	void FixedUpdate() {
 		if (!isDead) {
 
+			// if bean has more than one building material in inventory, add to its house
+			// will need to check if the bean's current house is build already. Then material will
+			// not be picked up.
 			if(blockMaterial > 0) {
 				buildHouse();
 			}
@@ -113,12 +115,19 @@ public class BeanLife : MonoBehaviour {
 			else
 				rigidbody2D.AddForce (new Vector2 (moveSpeed, 0));
 
+			// physics engine update tick
 			tick++;
 		}
 	}
 
+
+	// performs the beans side of building a house
 	void buildHouse() {
-		Debug.Log ("Building house...");
+		//Debug.Log ("Building house...");
+		// if the current house does not exist, create a new house at the bean's current world location.
+		// The house object will be created using the House class, which will handle the physical construction of the house.
+		// Place the first block that triggered this method call.
+		// And if the house does exist, add a block to it.
 		if (house == null) {
 			houseLoc = new Vector2(this.transform.position.x, this.transform.position.y);
 			house = new House();
@@ -131,7 +140,10 @@ public class BeanLife : MonoBehaviour {
 		blockMaterial--;
 	}
 
+	// GUI drawing method.
 	void OnGUI() {
+
+		// draw a label for the bean's house location
 		if (hasHouse) {
 			// draw house
 
@@ -148,8 +160,12 @@ public class BeanLife : MonoBehaviour {
 
 	}
 
-
+	// method to handle the age increase of the bean.
+	// Also handles the chance of death of the bean.
 	void aging() {
+
+		if(!isDead)
+			age++; 
 		if (!isAdult && age >= 18) {
 			isAdult = true;
 			if(isMale)
@@ -158,8 +174,6 @@ public class BeanLife : MonoBehaviour {
 				sr.sprite = femaleSprite; 
 			resizeCollider ();
 		}
-		if(!isDead)
-			age++; 
 
 		// FIX DEATH
 		if(age >= 18) {
@@ -177,14 +191,20 @@ public class BeanLife : MonoBehaviour {
 		}
 	}
 
+	// Deletes the game object from the game world, and I THINK deletes 
 	void destroy() {
 		Destroy (this.gameObject);
 	}
 
 
+	// Entering a collision with ANY other game object in the world.
 	void OnCollisionEnter2D(Collision2D col) {
+
+		// check if the collision object is another bean
 		if (col.gameObject.name.Contains ("Bean")) {
 			colliding = true;
+
+			// check pregnancy requirements, if all is fine, have a create a new bean.
 			if (col.gameObject.GetComponent<BeanLife> ().isMale && col.gameObject.GetComponent<BeanLife> ().isAdult && !isMale && isAdult && !givenBirth) {
 				if(!col.gameObject.GetComponent<BeanLife> ().beanName.Equals (fatherName)) {
 					Debug.Log ("Creating babby at: " + col.contacts [0].point);
@@ -195,6 +215,8 @@ public class BeanLife : MonoBehaviour {
 				}
 			}
 		}
+
+		// check if the collision object is a stone block material.
 		if (col.gameObject.name.Contains ("StoneBlock")) {
 			if(!col.gameObject.GetComponent<StoneBlock>().partOfHouse) {
 				Destroy(col.gameObject);
@@ -204,6 +226,7 @@ public class BeanLife : MonoBehaviour {
 	}
 
 
+	// Not sure if this works like I want it to... but check for continued collision with an object.
 	void OnCollisionStay2D(Collision2D col) {
 		if (col.gameObject.name.Contains ("Bean")) {
 			if (col.gameObject.GetComponent<BeanLife> ().isMale == isMale)
@@ -213,13 +236,15 @@ public class BeanLife : MonoBehaviour {
 		}
 	}
 
-
+	// Collision exited.
 	void OnCollisionExit2D(Collision2D col) {
 		if (col.gameObject.name.Contains ("Bean")) {
 			colliding = false;
 		}
 	}
 
+	// resize the 2D collider to fit different bean sprite sizes. 
+	// Currently hard coded, would like to make it base off the size of the sprite if possible. Should be.
 	void resizeCollider() {
 		CircleCollider2D c = this.gameObject.GetComponent<CircleCollider2D> ();
 		if(isAdult)
@@ -228,6 +253,11 @@ public class BeanLife : MonoBehaviour {
 			c.radius = 0.19F;
 	}
 
+
+	// grabs a name from one of two text files, depending on bean gender, and assings it to the bean.
+	// A BEAN CURRENTLY WILL BE ASSIGNED A 100% unique name, no two beans will have the same name.
+	// this will prob change if last names are added.
+	// The method itself is straight forward, will not explain it.
 	private void generateName() {
 		TextAsset txt = null;
 		string content = "";
@@ -261,7 +291,11 @@ public class BeanLife : MonoBehaviour {
 		}
 	}
 
+
+	// Object clicked on by mouse.
 	void OnMouseDown() {
+
+		// Select and deselect bean, sends a selected object to gameStats in order to display bean debug info over bean.
 		if (isSelected) {
 			isSelected = false;
 			gameStats.deselect ();
@@ -282,7 +316,7 @@ public class BeanLife : MonoBehaviour {
 
     void playSound(int clip)
     {
-        audio.clip = audioClip[clip];
+        //audio.clip = audioClip[clip];
     }
 
 	public void setMother(string s) { motherName = s; }
